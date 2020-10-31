b7b50567975c84fd694ff3bacac206f1fa091b77
wikky2012 <wicliferono@gmail.com>
2020-10-31 20:03:30 +0300

    create first API endpoint

--- restapi/views.py
+++ restapi/views.py
@@ -11,6 +11,24 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET', 'POST', 'DELETE'])
 def api_list(request):
+    if request.method == 'GET':
+        tutorials = Api.objects.all()
+        
+        title = request.GET.get('title', None)
+        if title is not None:
+            tutorials = tutorials.filter(title__icontains=title)
+        
+        tutorials_serializer = ApiSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
+        # 'safe=False' for objects serialization
+
+    elif request.method == 'POST':
+        tutorial_data = JSONParser().parse(request)
+        tutorial_serializer = ApiSerializer(data=tutorial_data)
+        if tutorial_serializer.is_valid():
+            tutorial_serializer.save()
+            return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     # GET list of apis, POST a new api, DELETE all apis
  
  
@@ -27,3 +45,4 @@ def api_detail(request, pk):
         
 @api_view(['GET'])
 def api_list_published(request):
+     # GET all published apis
